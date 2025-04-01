ef5bcc01eaec44a8f03071a8d5f280dc6d9de55e
ErnestoAGEV <vevelerne@gmail.com>
2025-04-01 09:55:03 -0600

    Argeue visualizar la contraseña

--- gestion-inventario/js/auth.js
+++ gestion-inventario/js/auth.js
@@ -58,3 +58,18 @@ function logout() {
     localStorage.removeItem("currentUser");
     window.location.href = "../pages/login.html";
 }
+
+document.getElementById("toggle-password").addEventListener("click", () => {
+    const passwordInput = document.getElementById("password");
+    const passwordIcon = document.getElementById("password-icon");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        passwordIcon.classList.remove("bi-eye");
+        passwordIcon.classList.add("bi-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        passwordIcon.classList.remove("bi-eye-slash");
+        passwordIcon.classList.add("bi-eye");
+    }
+});
